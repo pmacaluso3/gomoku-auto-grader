f96ff84dc7aa52c2393f3d70bf093b83d4de106b
PeterMacaluso <famous.pete.macaluso@gmail.com>
2023-10-09 15:44:09 -0700

    goal notes

---
+++ README.md
@@ -0,0 +1,14 @@
+# Semi-automated gomoku grading
+
+Usage goals:
+- set up a .env file w/ creds
+- download a folder of ungraded applicant submission zips from admin frontend
+- `node test-script.js`
+- check any failures manually & edit them in admin frontend if needed
+- `node cleanup-script.js`
+
+Other areas:
+- changing & deploying backend
+- changing & deploying frontend
+- admin use of frontend
+    - creating applicants
--- test-script.js
+++ test-script.js
@@ -14,6 +14,7 @@ fs.readdir(__dirname, (error, files) => {
     if (error) {
         console.log(error)
     } else {
+        // generate a gradingBatch id?
         for (const file of files) {
             if (file.endsWith(".zip")) {
                 const copyTo = `${file}-test`
@@ -21,7 +22,7 @@ fs.readdir(__dirname, (error, files) => {
                 run(`Copy-Item -R ${testSuiteFolder} ${copyTo}`)
                 run(`Expand-Archive -Path ${file} -DestinationPath ${unzipTo}`)
                 // in this current submission folder that we're iterating through:
-                // set env vars: applicant id, submission id, basic auth creds for report backend? 
+                // set env vars: backend url, applicant id, submission id, gradingBatch id?, basic auth creds for report backend? 
                 // reload maven dependencies
                 // run all junit tests
             }
