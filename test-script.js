9ad9401801b18826b983739b40429f48d4dade70
PeterMacaluso <famous.pete.macaluso@gmail.com>
2023-10-09 16:07:39 -0700

    put processed & unprocessed submissions into their own folders

--- test-script.js
+++ test-script.js
@@ -1,26 +1,33 @@
 const fs = require("fs")
 const { exec } = require("child_process")
-const testSuiteFolder = "gomoku-test-suite"
+const { stderr } = require("process")
+const applicantZipsFolder = `${__dirname}/applicant-zips`
+const unzippedSubmissionsFolder = `${__dirname}/unzipped-submissions`
+const testSuiteFolder = `${__dirname}/gomoku-test-suite`
 const unzipDestination = "src/main/java/learn/gomoku"
 
 const run = (cmd) => {
     exec(`powershell -Command "${cmd}"`, (err, stdErr) => {
-        console.log("err: ", err)
-        console.log("stdErr: ", stdErr)
+        if (err) {
+            console.log("err: ", err)
+        }
+        if (stderr && stdErr.length > 0) {
+            console.log("stdErr: ", stdErr)
+        }
     })
 }
 
-fs.readdir(__dirname, (error, files) => {
+fs.readdir(applicantZipsFolder, (error, files) => {
     if (error) {
         console.log(error)
     } else {
         // generate a gradingBatch id?
         for (const file of files) {
             if (file.endsWith(".zip")) {
-                const copyTo = `${file}-test`
+                const copyTo = `${unzippedSubmissionsFolder}/${file.replace(".zip", "")}`
                 const unzipTo = `${copyTo}/${unzipDestination}`
                 run(`Copy-Item -R ${testSuiteFolder} ${copyTo}`)
-                run(`Expand-Archive -Path ${file} -DestinationPath ${unzipTo}`)
+                // run(`Expand-Archive -Path ${file} -DestinationPath ${unzipTo}`)
                 // in this current submission folder that we're iterating through:
                 // set env vars: backend url, applicant id, submission id, gradingBatch id?, basic auth creds for report backend? 
                 // reload maven dependencies
